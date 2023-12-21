d2166d6e3f5a3faf1d58098dfbb945143b59bffb
marinovivianUPB <101936218+marinovivianUPB@users.noreply.github.com>
2023-12-21 11:10:25 -0400

    Update Date.js

--- src/utils/Date.js
+++ src/utils/Date.js
@@ -7,7 +7,7 @@ export const stringDate = () =>{
 }
 
 export const dateToString = (date) =>{
-    const day = String(date.getDate()).padStart(2, '0');
+    const day = String(date.getDate()+1).padStart(2, '0');
     const month = String(date.getMonth()+1).padStart(2, '0');
     const year = String(date.getFullYear());
     return year+"-"+month+"-"+day;
